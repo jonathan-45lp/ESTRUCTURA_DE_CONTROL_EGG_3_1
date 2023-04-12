49c17813d198b54f3af6b2276837bf5769c15bf2
Jonathan <jonathanemma41@gmail.com>
2023-04-11 23:12:03 -0300

    commit ESTRUCTURA DE CONTROL

---
+++ src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_8.java
@@ -0,0 +1,10 @@
+
+
+package com.mycompany.estructura_de_control_3_1;
+
+public class EJERCICIO_3_1_8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/estructura_de_control_3_1/ESTRUCTURA_DE_CONTROL_3_1.java
@@ -0,0 +1,10 @@
+
+
+package com.mycompany.estructura_de_control_3_1;
+
+public class ESTRUCTURA_DE_CONTROL_3_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
