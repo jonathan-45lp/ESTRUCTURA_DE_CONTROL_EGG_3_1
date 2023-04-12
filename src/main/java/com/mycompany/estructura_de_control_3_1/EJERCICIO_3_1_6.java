dfceb172cc14e9c083e8c1f074b50bfc0d1c4028
Jonathan <jonathanemma41@gmail.com>
2023-04-12 00:28:45 -0300

    commit ejercicio 6 y 7 resuelto

--- src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_6.java
+++ src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_6.java
@@ -1,10 +1,26 @@
-
-
 package com.mycompany.estructura_de_control_3_1;
-
+import java.util.Scanner;
 public class EJERCICIO_3_1_6 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("INGRESE NUMERO 1: ");
+        int n1 = leer.nextInt();
+        
+        System.out.println("INGRESE NUMERO 2: ");
+        int n2 = leer.nextInt();
+        
+        if(n1 > 25 && n2 > 25){
+            System.out.println("LOS DOS NUMERO SON MAYOR QUE 25: " + " N1 " +n1 + " N2 " + n2);
+        }else if(n1 > 25){
+            System.out.println("EL NUMERO N1 ES MAYOR QUE 25: " + n1);
+        }else if(n2 > 25){
+            System.out.println("EL NUMERO N2 ES MAYOR QUE 25: " + n2);
+        }else{
+            System.out.println("NINGUNO DE LOS NUMERO SON MAYORES A 25");
+        }
+        
+        
     }
 }
--- src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_7.java
+++ src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_7.java
@@ -1,10 +1,31 @@
-
-
 package com.mycompany.estructura_de_control_3_1;
-
+import java.util.Scanner;
 public class EJERCICIO_3_1_7 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.print("INGRESE UNA OPCION DESDE 1 AL 4 LA OPCION ES : ");
+        int op = leer.nextInt();
+        
+        switch (op){
+            case 1:{ 
+                    System.out.println("LA BOMBA ES UNA BOMBA DE AGUA");
+                    break;}
+            case 2:{ 
+                    System.out.println("LA BOMBA ES UNA BOMBA DE GASOLINA");
+                    break;}
+            case 3:{ 
+                    System.out.println("LA BOMBA ES UNA BOMBA DE HORMIGON");
+                    break;}
+            case 4:{ 
+                    System.out.println("LA BOMBA ES UNA BOMBA DE PASTA ALIMENTICIA");
+                    break;}
+            default:{ 
+                    System.out.println("NO EXISTE UN VALOR VALIDO PARA TIPO DE BOMBA");
+            }
+            
+        
+        }
     }
 }
