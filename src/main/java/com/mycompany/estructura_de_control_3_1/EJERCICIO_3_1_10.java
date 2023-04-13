5c58bc720db7168e0d75f5c49195172f9de703bd
Jonathan <jonathanemma41@gmail.com>
2023-04-13 07:21:59 -0300

    ejercicio 10

--- src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_10.java
+++ src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_10.java
@@ -1,10 +1,22 @@
-
-
 package com.mycompany.estructura_de_control_3_1;
-
+import java.util.Scanner;
 public class EJERCICIO_3_1_10 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner leer = new Scanner(System.in);
+      
+       
+        
+        for(int i = 1; i <= 4; i++){
+            System.out.print("INGRESE EL VALOR EN LA POSICION " +i+ " : ");
+            int num = leer.nextInt();
+            
+            System.out.print(num + ": ");
+            
+            for(int j = 0; j < num; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
