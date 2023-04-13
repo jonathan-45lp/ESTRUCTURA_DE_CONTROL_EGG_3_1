10db3a0a859b7c3f90e474f778090c9267dd016f
Jonathan <jonathanemma41@gmail.com>
2023-04-13 01:05:51 -0300

    commit ejercicio 8 y 9

--- src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_8.java
+++ src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_8.java
@@ -1,10 +1,19 @@
-
-
 package com.mycompany.estructura_de_control_3_1;
+import java.util.Scanner;
 
 public class EJERCICIO_3_1_8 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.print("INGRESE NOTA: ");
+        int nota = leer.nextInt();
+        
+        
+        while( nota < 0 || nota > 10){
+            System.out.print("INGRESE NUEVA MENTE LA NOTA: ");
+            nota = leer.nextInt();
+        }
+        
     }
 }
--- src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_9.java
+++ src/main/java/com/mycompany/estructura_de_control_3_1/EJERCICIO_3_1_9.java
@@ -1,10 +1,27 @@
-
-
 package com.mycompany.estructura_de_control_3_1;
-
+import java.util.Scanner;
 public class EJERCICIO_3_1_9 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner leer = new Scanner(System.in);
+        
+        
+        int cont = 0;
+        int suma = 0;
+        do{
+            System.out.print("INGRES UN NUMERO: ");
+            int num = leer.nextInt();
+            cont = cont + 1;
+            if(num > 0){
+                suma = suma + num;
+            }else if( num == 0 ){
+                System.out.print("SE CAPTURO EL NUMERO CERO: " + num);
+                break;
+            }
+                
+        }while( cont != 20);
+        
+        System.out.println("LA SUMA ES: " + suma);
+        
     }
 }
